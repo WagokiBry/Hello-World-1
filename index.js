8f96b7e5ec65328307984134f17b7d0cd0d4437c
Bryan <bryanwagoki2016@gmail.com>
2026-01-04 15:13:59 +0300

    Create a fibonacci function

--- index.js
+++ index.js
@@ -16,38 +16,6 @@
 // }
 
 // console.log(sumTo(100))
-
-function sumTo(n){
-    if(n==0){
-        return 0
-    }
-    return n + sumTo(n-1)
+function fibonacci (n){
+  
 }
-
-console.log(sumTo(4))
-
-let list = {
-  value: 1,
-  next: {
-    value: 2,
-    next: {
-      value: 3,
-      next: {
-        value: 4,
-        next: null
-      }
-    }
-  }
-};
-
-function printList(list) {
-
-  alert(list.value); // output the current item
-
-  if (list.next) {
-    printList(list.next); // do the same for the rest of the list
-  }
-
-}
-
-printList(list);
