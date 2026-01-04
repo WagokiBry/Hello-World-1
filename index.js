fb815f425705d8fe8049d698ab1f39cffee7935c
Bryan <bryanwagoki2016@gmail.com>
2026-01-04 06:58:07 +0300

    Create a function that adds numbers using a for loop

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
-let list = { value: 1 };
-list.next = { value: 2 };
-list.next.next = { value: 3 };
-list.next.next.next = { value: 4 };
+// let list = { value: 1 };
+// list.next = { value: 2 };
+// list.next.next = { value: 3 };
+// list.next.next.next = { value: 4 };
 
-// prepend the new value to the list
-list = { value: "new item", next: list };
+// // prepend the new value to the list
+// list = { value: "new item", next: list };
+// console.log(list.next.value)
+
+function sumTo (n){
+let sum = 0;
+for(let i = 1;i <=n ; i++){
+    sum+=i
+}
+return sum
+}
+
+console.log(sumTo(100))
+
