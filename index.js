5f401eb9f754df6e3030106cc4c7c9d44def0d58
Bryan <bryanwagoki2016@gmail.com>
2026-01-04 05:43:53 +0300

    Add an object company that has sub-departments

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-function delay(ms){
-    return new Promise((res,rej)=>{
-        setTimeout(()=>{
-res("good")
-        },ms)
-    })
-}
-
-console.log(delay(1000).then(() => alert('runs after 3 seconds')));
+let company = { // the same object, compressed for brevity
+  sales: [{name: 'John', salary: 1000}, {name: 'Alice', salary: 1600 }],
+  development: {
+    sites: [{name: 'Peter', salary: 2000}, {name: 'Alex', salary: 1800 }],
+    internals: [{name: 'Jack', salary: 1300}]
+  }
+};
