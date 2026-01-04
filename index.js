856801486704762f43dfeba0036bf219ddb477ea
Bryan <bryanwagoki2016@gmail.com>
2026-01-04 07:10:14 +0300

    Create a recursive function that adds numbers up to a certain value

--- index.js
+++ index.js
@@ -7,13 +7,21 @@
 // list = { value: "new item", next: list };
 // console.log(list.next.value)
 
-function sumTo (n){
-let sum = 0;
-for(let i = 1;i <=n ; i++){
-    sum+=i
-}
-return sum
+// function sumTo (n){
+// let sum = 0;
+// for(let i = 1;i <=n ; i++){
+//     sum+=i
+// }
+// return sum
+// }
+
+// console.log(sumTo(100))
+
+function sumTo(n){
+    if(n==0){
+        return 0
+    }
+    return n + sumTo(n-1)
 }
 
-console.log(sumTo(100))
-
+console.log(sumTo(4))
